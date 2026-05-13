ddf9fa4423d8c05d6a6c058fad932e6df2f856bb
GabrieleChiorrini <gabrielechiorrini11@gmail.com>
2026-05-13 16:35:34 +0200

    CU11
    Co-Authored-By: DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>

--- Services/gestorePagamento.py
+++ Services/gestorePagamento.py
@@ -1,2 +1,19 @@
+from datetime import datetime
+from Models import Pagamento, Cliente
+from Repo import ClienteRepository, PagamentoRepository
 
+
+class GestorePagamento ():
+    def __init__(self, clienteRepo: ClienteRepository, pagamentoRepo: PagamentoRepository):
+        self._clienteRepo = clienteRepo
+        self._pagamentoRepo = pagamentoRepo
+
+    def registrarePagamento (self, pagamentoId: str, clienteId: str, importo: float, data: datetime) -> str:
+        cliente =  self._clienteRepo.trovaPerId(clienteId)
+        if cliente is None:
+            return 'Cliente non trovato'
+        pagamento = Pagamento(self._pagamentoRepo.newId(), importo, data, cliente)
+        self._pagamentoRepo.aggiungi(pagamento)
+        return 'Pagamento registrato'
+    
         
