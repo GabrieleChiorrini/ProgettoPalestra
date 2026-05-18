c32fbfca141e9276958838aacd8b1a29cba72616
DukeEdivad05 <75534398+DukeEdivad05@users.noreply.github.com>
2026-05-18 12:14:47 +0200

    error correction

--- Services/gestorePrenotazioni.py
+++ Services/gestorePrenotazioni.py
@@ -12,35 +12,49 @@ class GestorePrenotazioni:
         self._gestoreCapienza = gestoreCapienza
     
     def prenotaCorso(self, corsoId: str, clienteId: str) -> str:
-        corso: Corso = self._corsoRepo.trovaPerId(corsoId)
-        cliente: Cliente = self._clienteRepo.trovaPerId(clienteId)
+        corso = self._corsoRepo.trovaPerId(corsoId)
+        if corso is None:
+            return "Corso non trovato"
+        cliente = self._clienteRepo.trovaPerId(clienteId)
+        if cliente is None:
+            return "Cliente non trovato"
 
         if not self._gestoreCapienza.controllaCapienzaCorso(corsoId):
             return f"Il corso {corso.get_nome()} è pieno"
         
-        prenotazione = PrenotazioneCorso(cliente, corso, self._prenotazioneRepo.newId())
-        self._prenotazioneRepo.aggiungi(prenotazione)
-        self._prenotazioneRepo.salva()
+        prenotazione = PrenotazioneCorso(cliente, corso, self._prenotazioneCorsoRepo.newId())
+        self._prenotazioneCorsoRepo.aggiungi(prenotazione)
+        self._prenotazioneCorsoRepo.salva()
 
         corso.set_iscritti(corso.get_iscritti().append(cliente))
         self._corsoRepo.salva()
         return f'Prenotazione per il corso {corso.get_nome()} effettuata con successo!'
 
     def eliminaPrenotazioneSalaPesi(self, prenotazioneId: str, clienteId: str) -> str:
-        prenotazione = self._prenotazioneRepo.trovaPerId(prenotazioneId)
+        prenotazione = self._prenotazioneSalaPesiRepo.trovaPerId(prenotazioneId)
+        if prenotazione is None:
+            return "Prenotazione non trovata"
         cliente = self._clienteRepo.trovaPerId(clienteId)
+        if cliente is None:
+            return "Cliente non trovato"
 
         self._prenotazioneSalaPesiRepo.rimuovi(prenotazione)
+        return "Prenotazione eliminata"
     
     def eliminaPrenotazioneCorso(self, prenotazioneId: str, clienteId: str) -> str:
-        prenotazione = self._prenotazioneRepo.trovaPerId(prenotazioneId)
+        prenotazione = self._prenotazioneCorsoRepo.trovaPerId(prenotazioneId)
+        if prenotazione is None:
+            return "Prenotazione non trovata"
         cliente = self._clienteRepo.trovaPerId(clienteId)
+        if cliente is None:
+            return "Cliente non trovato"
 
         self._prenotazioneCorsoRepo.rimuovi(prenotazione)
 
         corso: Corso = prenotazione.get_corso()
         corso.set_iscritti(corso.get_iscritti().remove(cliente))
         self._corsoRepo.salva()
+        return "Prenotazione eliminata"
     
     def prenotareSalaPesi(self, fasciaOrariaId: str, clienteId: str) -> str:
         fasciaOraria: FasciaOraria = self._fasciaOrariaRepo.trovaPerId(fasciaOrariaId)
