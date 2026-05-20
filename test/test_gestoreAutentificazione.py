22dad38c3e3ede9f8a0b0607c551bcaf46f77fb3
dafnemill <63858413+dafnemill@users.noreply.github.com>
2026-05-20 13:42:26 +0200

    Create test_gestoreAutentificazione.py

---
+++ test/test_gestoreAutentificazione.py
@@ -0,0 +1,5 @@
+import unittest, os
+from test import entita_finte as ef
+from Services import GestoreCliente
+from Repo import ClienteRepository, CertificatoMedicoRepository
+from datetime import datetime, time
